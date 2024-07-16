bae269cce7d10a683c8ab46425dd7038aa222c6e
ROBIN THOMAS <robin.19thomas@gmail.com>
2024-07-16 16:59:10 +0200

    chore: ajout prettier

--- back/dist/index.js
+++ back/dist/index.js
@@ -6,8 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+    res.send("Hello World!");
 });
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);

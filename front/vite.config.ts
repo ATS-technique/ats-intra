0f6d7b1fe23c2558a97776dbcbf2a8985ed09009
Robin THOMAS <technique@ats-serrurerie.com>
2025-06-10 12:43:12 +0200

    fix: vite config

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,19 +1,12 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
+export default defineConfig({
   // https://vitejs.dev/config/
-  return {
     base: "/",
     plugins: [react()],
-    server: {
-      historyApiFallback: true,
-      port: env.VITE_PORT ? Number(env.VITE_PORT) : 4000,
-    },
     build: {
       outDir: "dist",
       emptyOutDir: true,
     },
-  };
 });

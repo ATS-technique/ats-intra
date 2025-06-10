38b0acee1e36cfef041996c4005d075bab5632d7
Robin THOMAS <technique@ats-serrurerie.com>
2025-06-10 16:56:21 +0200

    fix: vercel deploy

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  // https://vitejs.dev/config/
-  base: "/",
   plugins: [react()],
   build: {
     outDir: "dist",
     emptyOutDir: true,
   },
+  base: "/",
 });

d3394c55af8ec9dfe3a66d72a5462ca5d6c5e70a
Robin THOMAS <technique@ats-serrurerie.com>
2025-06-10 12:44:11 +0200

    fix: deployment conf

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,10 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   // https://vitejs.dev/config/
-    base: "/",
-    plugins: [react()],
-    build: {
-      outDir: "dist",
-      emptyOutDir: true,
-    },
+  base: "/",
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
 });

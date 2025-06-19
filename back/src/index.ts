8bf686a1bb2a88c6ad90227f8aee805c8d6320b3
Robin THOMAS <technique@ats-serrurerie.com>
2025-06-19 12:32:26 +0200

    fix(chore): cors fix

--- back/src/index.ts
+++ back/src/index.ts
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 3000;
 const allowedOrigins = [
   process.env.FRONTEND_URL || "http://localhost:4000",
   "https://ats-serrurerie.com",
-  "http://ats-mettalerie-serrurerie.com",
+  "http://ats-mettalerie-serrurerie.fr",
   "http://127.0.0.1:5501",
 ];
 
